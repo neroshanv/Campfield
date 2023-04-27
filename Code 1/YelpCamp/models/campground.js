a553a655ddbc50e595694d2d4f6debed02f45e80
neroshanv <neroshan.n@gmail.com>
2023-04-27 18:25:49 -0400

    created review app and connected it to campground

--- Code 1/YelpCamp/models/campground.js
+++ Code 1/YelpCamp/models/campground.js
@@ -3,12 +3,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const CampgroundSchema = new Schema ({
+const CampgroundSchema = new Schema({
     title: String,
     image: String,
     price: Number,
     Description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId
+            ref: 'Review'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Camground', CampgroundSchema);
