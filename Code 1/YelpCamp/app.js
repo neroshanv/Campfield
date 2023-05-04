791750759229f7c5b8f96e43c6589940142967a2
neroshanv <neroshan.n@gmail.com>
2023-05-04 18:52:38 -0400

    created a route for user to store data

--- Code 1/YelpCamp/app.js
+++ Code 1/YelpCamp/app.js
@@ -13,7 +13,9 @@ const ExpressError = require('./utils/ExpressError');
 // passport ( pretend we installed passport) ( npm i passport passport-local passport-local-mongoose)
 // will have issues when installing this on mongoose>7
 // you have to runn npm install mongoose@"<7.0.0" and run npm i passport passport-local passport-local-mongoose again)
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 // -----------------------------------------------------
 const methodOverride = require('method-override');
 
@@ -61,13 +63,30 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+// we would like to use LocalStrategy that we downloaded and required 
+// and for LocalStrategy the authenticate method is going to be located in user model called authenticate
+passport.use(new LocalStrategy(User.authenticate()));
+// telling passport how do we get data or how to store a user in the session
+passport.serializedUser(User.serializedUser())
+// telling passport how do we get a user out of the session
+passport.deserializeUser(User.deserializeUser())
+
 
 app.use((req, res, next => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
-
     next();
 }))
+
+// a route to create new user
+app.get('/fakeUser', async (req, res) => {
+    const user = new user({ email: 'colt@gmail.com,', username: 'colt' })
+    // chicken is password
+    const newUser = await User.register(user, 'chicken')
+    res.send(newUser);
+})
 
 app.use('/campgrounds', campgrounds)
 app.use('./campgrounds/:idreviews')
