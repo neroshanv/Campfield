d2e20bd05b3c46b240b89772ab9218da7c6109aa
neroshanv <neroshan.v@hotmail.com>
2023-05-15 23:13:22 -0400

    added more code to controller folder

--- Code 1/YelpCamp/routes/campgrounds.js
+++ Code 1/YelpCamp/routes/campgrounds.js
@@ -10,30 +10,29 @@ const { isLoggedIn, IsAuthor, validateCampground } = require('../middleware');
 
 const Campground = require('../models/campground');
 
-
-
-// list of all campgrounds
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+    // list of all campgrounds
+    .get(catchAsync(campgrounds.index))
+    // creation of campground
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
 // new form and serve that somewhere as a GET request and then create route as a POST request
 router.get('/new', IsLoggedIn, campgrounds.renderNewForm)
 
+router.route('/:id')
+    // implement show route which is eventually going to be a details page for campground
+    .get('/:id', catchAsync(campgrounds.showCampground))
+    // sending a real post request that we are faking with methodOverride
+    .put('/:id', IsLoggedIn, IsAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    // delete button
+    .delete('/:id', IsLoggedIn, IsAuthor, catchAsync(campgrounds.deleteCampgrounds));
+
+
+
+
+
 // end point where the form is submitted too
-// creation of campground
-router.post('/', IsLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-// implement show route which is eventually going to be a details page for campground
-router.get('/:id', catchAsync(campgrounds.showCampground));
-
 router.get('/:id/edit', IsLoggedIn, IsAuthor, catchAsync(campgrounds.renderEditForm));
-
-
-
-// sending a real post request that we are faking with methodOverride
-router.put('/:id', IsLoggedIn, IsAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-// delete button
-router.delete('/:id', IsLoggedIn, IsAuthor, catchAsync(campgrounds.deleteCampgrounds));
 
 
 
