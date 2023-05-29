b1f3e9d10d51623662f8f342251579e5118d3cc6
neroshanv <neroshan.v@hotmail.com>
2023-05-28 23:01:11 -0400

    changes from instructor

--- Code 1/YelpCamp/app.js
+++ Code 1/YelpCamp/app.js
@@ -23,6 +23,7 @@ const ExpressError = require('./utils/ExpressError');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
 // -----------------------------------------------------
 const methodOverride = require('method-override');
 
@@ -55,6 +56,9 @@ app.set('views', path.join(__dirname, 'view'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(_dirname, 'public')))
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
 
 const sessionConfig = {
     secret: 'Thisshouldbeabettersecret',
@@ -67,8 +71,53 @@ const sessionConfig = {
     }
 }
 
-app.use(session(sessionConfig))
+app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -90,17 +139,11 @@ app.use((req, res, next => {
 
 
 
-// a route to create new user
-app.get('/fakeUser', async (req, res) => {
-    const user = new user({ email: 'colt@gmail.com,', username: 'colt' })
-    // chicken is password
-    const newUser = await User.register(user, 'chicken')
-    res.send(newUser);
-})
-
 app.use('/', userRoutes);
 app.use('/campgrounds', campgroundsRoutes)
 app.use('./campgrounds/:idreviews', reviewRoutes)
+
+
 
 app.get('/', (req, res) => {
     res.render('home')
