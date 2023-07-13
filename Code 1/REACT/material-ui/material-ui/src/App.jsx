60aa3139a4c8bef52b7c3a9bf161e0798471ee56
neroshanv <neroshan.v@hotmail.com>
2023-07-13 10:13:42 -0400

    material-ui app and packages such as icon and font installed

---
+++ Code 1/REACT/material-ui/material-ui/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Button from '@mui/material/Button';
+
+export default function BasicButtons() {
+  function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+      <Button variant="contained">Contained</Button>
+    )
+  }
+
+  export default App
