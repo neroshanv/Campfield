925c5f30489b522596ad1f16a69870d2295cba94
neroshanv <neroshan.v@hotmail.com>
2023-12-20 08:24:44 -0500

    Deleted mutiple course files

--- REACT course/01-starting-project/01-starting-project/src/App.jsx
+++ REACT course/01-starting-project/01-starting-project/src/App.jsx
@@ -1,4 +1,8 @@
 import reactImg from './assets/react-core-concepts.png';
+// Do not have access to these files so far //
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
@@ -8,6 +12,10 @@ function genRandomInt(max) {
 }
 
 function App() {
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'   - These could be our 4 identifiers that should be recieved through that paramenter. //
+    console
+  }
 
   const description = reactDescriptions[genRandomInt(2)];
   return (
