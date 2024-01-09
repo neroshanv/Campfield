b179e756918929986058e49e689e6fc3bd4cede8
neroshanv <neroshan.v@hotmail.com>
2024-01-09 08:30:17 -0500

    Investment Caluculator

--- REACT course/Essentials Practice/01-starting-project/src/App.jsx
+++ REACT course/Essentials Practice/01-starting-project/src/App.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  // function that should trigger whenever we change the value in these output fields "Initial Investment, Annual Investment, Expected return, Duration"
+  // Goal: one generic function that can be connected to all inputs
+
+  // for updating state where we get the previous userinput
+  // and where we then return our updated state object into this new object so that we copy all the old values into this new object
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...preUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
+    </>
+  );
 }
 
-export default App
+export default App;
