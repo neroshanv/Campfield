7b2c9a90bec2ed3cadf36dce4165e0a4adb1c1e9
neroshanv <neroshan.v@hotmail.com>
2024-03-27 07:57:38 -0400

    Redux intro

---
+++ REACT course/Redux/redux-demo.js
@@ -0,0 +1,18 @@
+const redux = require('redux');
+
+const counterReducer = (state = { counter: 0 }, action) => {
+    return {
+        counter: state.counter + 1
+    };
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+    const latestState = store.getState();
+
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: 'increment' });
