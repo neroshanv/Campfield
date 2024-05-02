81ed0cd7134a5797862e6d78df5b57430c39861b
neroshanv <neroshan.v@hotmail.com>
2024-05-02 07:09:17 -0400

    Building Multi-Page SPA with React Router
    submitting up til working with action() functions.

---
+++ REACT course/12-adv-starting-project/frontend/src/pages/EditEvent.js
@@ -0,0 +1,12 @@
+import { useRouteLoaderData } from "react-router-dom";
+
+import EventForm from '../components/EventForm';
+
+function EditEventPage() {
+    const data = useRouteLoaderData('event-detail');
+    const event = data.event;
+    return <EventForm event={data.event} />;
+
+}
+
+export default EditEventPage;
---
+++ REACT course/12-adv-starting-project/frontend/src/pages/NewEvent.js
@@ -0,0 +1,42 @@
+import { json, redirect } from 'react-router-dom';
+
+import EventForm from "../components/EventForm";
+
+function NewEventPage() {
+
+    function NewEventPage() {
+        function submitHandler(event) {
+            event.preventDefault();
+
+        }
+    }
+    return <EventForm />
+}
+
+export default NewEventPage;
+
+export async function action({ request, params }) {
+    const data = request.formData();
+
+    const eventData = {
+        title: data.get('title'),
+        image: data.get('image'),
+        date: data.get('date'),
+        description: data.get('description'),
+    }
+
+    fetch('http://localhost:8080/events', {
+
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(eventData)
+    });
+
+    if (!response.ok) {
+        throw json({ message: 'Could not save event.' }, { status: 500 });
+    }
+
+    return redirect('/events');
+}
