b296ea77bd79a817ff20b252f3052c7afa7b252b
neroshanv <neroshan.v@hotmail.com>
2024-06-13 06:57:55 -0400

    Updated pages
    MealDetailPages updated with current code

--- REACT course/foodies-starting-project/app/lib/meals.js
+++ REACT course/foodies-starting-project/app/lib/meals.js
@@ -8,3 +8,8 @@ export async function getMeals() {
     throw new Error('Loading meals failed');
     return db.prepare('SELECT * FROM meals').all();
 }
+
+export function getMeals(slug) {
+
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
